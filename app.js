e18b650dfc491ae0cb5f37bc36dbd4261bc5eb5a
Arpit Mahajan <arpitmahajan32@gmail.com>
2025-08-15 12:10:46 +0530

    Server Setup Is Done Using Socket IO !

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const {Chess} = require('chess.js');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const server = http.createServer(app);
+const io = socket(server)
+
+
+const chess = new Chess();
+
+let players = {};
+let currentPlayer = 'W';
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get('/', (req, res) => {
+    res.render('index', {title : "Chess Game"});
+});
+
+io.on("connection", function(uniquesocket){
+    console.log("New player connected");
+});
+
+
+
+
+
+
+server.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
+
+
+
