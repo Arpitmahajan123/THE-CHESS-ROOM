d12afd322d684a1c07be4aebc6199c01a84d4fef
Arpit Mahajan <arpitmahajan32@gmail.com>
2025-08-16 13:24:42 +0530

    This Is Code For Creating The Chess Board Pattern !

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,6 +1,4 @@
 // Basic Socket IO setup
-
-
 // iss Line Ke Code Se Jitne Bhe User Join Honge Frontend Se Backend Mein Request Jayegi.
 
 const socket = io();
@@ -8,5 +6,42 @@ const socket = io();
 // Abhe Hum Ye Frontend Pe Bhejenge.
 
 const chess = new Chess();
+const boardElement = document.querySelector('.chessboard');
 
-const boardElement = document.getElementById('.chessboard');
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+
+const renderBoard = () => {
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach((row, rowindex) => {
+        row.forEach((square, squareindex) => {
+            const squareElement = document.createElement('div');
+            squareElement.classList.add(
+                "square",
+                (rowindex + squareindex) % 2 === 0 ? "light" : "dark",
+            );
+            squareElement.dataset.row = rowindex;
+            squareElement.dataset.col = squareindex;
+
+            if(square) {
+                const pieceElement = document.createElement("div"); 
+                pieceElement.classList.add('piece', square.color === 'W' ? "white" : "black");
+            }
+        });
+    });
+};
+
+
+const handelMove = (square, piece) => {
+
+}
+
+// For The Shape And Structure Of Chess Piece.
+const getPieceUnicode = (piece) => {
+
+}
+
+renderBoard();
